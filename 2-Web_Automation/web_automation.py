678f94c9e244fd7e6400f4b775170faf44654765
U003081 <gustavo.honorio@neon.com.br>
2024-01-03 15:59:16 -0300

    2 - Web Automation

---
+++ 2-Web_Automation/web_automation.py
@@ -0,0 +1,4 @@
+# Use Selenium to automate interactions with a website, such as logging in or submitting a form.
+# Write a script to scrape data from a website and save it to a file.
+
+# Importando modulos
